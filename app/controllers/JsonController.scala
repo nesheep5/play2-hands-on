5aec0af2b2f6047fefcd25a9fde754b3e2521d0c
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-08-08 09:22:02 +0900

    JSON形式用コントロー雛形作成

---
+++ app/controllers/JsonController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import play.api.mvc._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider)
+    extends Controller
+    with HasDatabaseConfigProvider[JdbcProfile] {
+
+  /**
+   * 一覧表示
+   */
+  def list = TODO
+
+  /**
+   * ユーザ登録
+   */
+  def create = TODO
+
+  /**
+   * ユーザ更新
+   */
+  def update = TODO
+
+  /**
+   * ユーザ削除
+   */
+  def remove(id: Long) = TODO
+}
